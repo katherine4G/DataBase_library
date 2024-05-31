a9d9f40178441426c5ed4a80d9b6d919689704d7
katherine4G <112660945+katherine4G@users.noreply.github.com>
2024-05-31 15:34:11 -0600

    Completation of MySql Connection and CRUD

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,11 +1,4 @@
 package com.example;
-//oracle
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
-
 
 ////////////////// ORACLE//////////////////////////////////////////
 
